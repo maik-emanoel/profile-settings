7d3a84a060116c6402d1dde9868a9210d50bbdd8
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-04 12:05:04 -0300

    add content

--- src/pages/Accessibility/Accessibility.jsx
+++ src/pages/Accessibility/Accessibility.jsx
@@ -1,9 +1,23 @@
+import { CaretRight } from "@phosphor-icons/react"
 import BackButton from "../../components/BackButton"
+import ToggleButton from "../../components/toggleButton/index"
 
 export default function Accessibility() {
     return (
-        <div className="container">
+        <div className="container accessibility">
             <BackButton />
+
+            <h3 className="page-title">Acessibilidade</h3>
+
+            <div className="wrapper">
+                <span>Tamanho de fonte</span>
+                <CaretRight />
+            </div>
+
+            <div className="wrapper">
+                <span>Leitor de tela</span>
+                <ToggleButton />
+            </div>
         </div>
     )
 }
