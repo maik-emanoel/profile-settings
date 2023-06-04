1fedd3346f94cb4b5ca229bb0e936ab6c76e30e2
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-04 12:03:51 -0300

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import PersonalData from "./pages/PersonalData/PersonalData";
 import LoginInfo from "./pages/LoginInfo/LoginInfo";
 import Accessibility from "./pages/Accessibility/Accessibility";
 import Notifications from "./pages/Notifications/Notifications";
+import LinkedDevices from "./pages/LinkedDevices/LinkedDevices"
+import LinkedAccounts from "./pages/LinkedAccounts/LinkedAccounts";
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
         <Route path="/login-info" element={<LoginInfo />} />
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/accessibility" element={<Accessibility />} />
+        <Route path="/linked-devices" element={<LinkedDevices />} />
+        <Route path="/linked-accounts" element={<LinkedAccounts />} />
       </Routes>
     </Router>
   )
