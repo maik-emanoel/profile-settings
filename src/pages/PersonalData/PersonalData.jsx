4f34ac47f0108ed75337dca5e3297798d5c578a8
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-04 11:09:08 -0300

    add content to personal data

--- src/pages/PersonalData/PersonalData.jsx
+++ src/pages/PersonalData/PersonalData.jsx
@@ -1,7 +1,31 @@
+import BackButton from '../../components/BackButton'
+
+// eslint-disable-next-line react/prop-types
 export default function PersonalData() {
     return (
         <div className="container">
-            <h1>Voce esta na pagina de dados pessoais</h1>
+            <BackButton />
+
+            <h3 className="page-title">Dados Pessoais</h3>
+            <div className="wrapper">
+                <p className="title">Nome de usuário</p>
+                <span className="content">@marvin.mckinney</span>
+            </div>
+
+            <div className="wrapper">
+                <p className="title">Número de telefone</p>
+                <span className="content">+55 11 4002-8922</span>
+            </div>
+
+            <div className="wrapper">
+                <p className="title">Data de nascimento</p>
+                <span className="content">01 de setembro de 2004</span>
+            </div>
+
+            <div className="wrapper">
+                <p className="title">País de origem</p>
+                <span className="content">Brasil</span>
+            </div>
         </div>
     )
 }
