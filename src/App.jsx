7d961bb4f337f5a22adacc161db916396930fd0a
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-04 10:58:53 -0300

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import PersonalData from "./pages/PersonalData/PersonalData";
-import LoginInfo from "./pages/LoginInfo/LoginInfo"
+import LoginInfo from "./pages/LoginInfo/LoginInfo";
+import Accessibility from "./pages/Accessibility/Accessibility";
+import Notifications from "./pages/Notifications/Notifications";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/personal-data" element={<PersonalData />} />
         <Route path="/login-info" element={<LoginInfo />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/accessibility" element={<Accessibility />} />
       </Routes>
     </Router>
   )
